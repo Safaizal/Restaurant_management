da1fdcc1ddc1acd78cb1c48a62030eb9a472305c
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-09-27 21:14:21 +0530

    I have modified the signup page to save the user in localstorage and post it to the API with help of postman and axios.

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -5,18 +5,43 @@
   <h1>Sign Up</h1>
 
   <div class="register">
-    <input type="text" placeholder="Name">
-    <input type="email" placeholder="Email">
-    <input type="password" placeholder="Password">
+    <input type="text" v-model="name" placeholder="Name">
+    <input type="email" v-model="email" placeholder="Email">
+    <input type="password" v-model="password" placeholder="Password">
 
-    <button>Sign Up</button>
+    <button v-on:click="SignUp">Sign Up</button>
   </div>
 
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-  name : "SignUp"
+  name : "SignUp",
+  data () {
+    return {
+      name : "",
+      email : "",
+      password : ""
+    }
+  },
+  methods : {
+    async SignUp () {
+      let result = await axios.post("http://localhost:3000/users", {
+        email:this.email,
+        name:this.name,
+        password:this.password
+      });
+
+      console.warn(result)
+      if(result.status == 201){
+        alert("Successfully Signed up...")
+        localStorage.setItem("user_info", JSON.stringify(result.data))
+      }
+    }
+  }
 }
 </script>
 
