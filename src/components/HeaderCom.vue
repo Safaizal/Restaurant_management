87778a4d310f2a24714b779ca6bf5f1822b57f8f
Safaizal <127269918+Safaizal@users.noreply.github.com>
2025-09-30 17:56:40 +0530

    Created the Header component

---
+++ src/components/HeaderCom.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Update Restaurant</a>
+        <a href="#">Logout</a>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name : "HeaderCom"
+}
+
+</script>
+
+<style>
+.nav{
+    background-color: black;
+    overflow: hidden;
+}
+.nav a{
+    float: left;
+    color: white;
+    padding: 15px 15px;
+    text-align: center;
+    text-decoration: none;
+    margin-right: 10px;
+}
+.nav a:hover{
+    background-color: white;
+    color: black;
+    transition: 500ms;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,22 @@
 <template>
 
+  <HeaderCom>
+
+  </HeaderCom>
+
   <h1> Home page </h1>
   <router-link to="/login"> LogOut </router-link>
 
 </template>
 <script>
 
+import HeaderCom from './HeaderCom.vue';
+
 export default {
   name: "HomePage",
+  components : {
+    HeaderCom
+  },
   mounted () {
     let user = localStorage.getItem('user_info')
     if(!user){
